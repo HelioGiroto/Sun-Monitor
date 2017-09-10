ccc472a6c9691a08cd703430690959c1a9cf2ab5
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-09-09 21:48:58 -0300

    Add files via upload

---
+++ Sun-monitor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+kindex=$(curl -s 'http://services.swpc.noaa.gov/products/noaa-planetary-k-index.json' | jq '.[-1][1]' | sed 's/"//g')
+
+if [[ "$kindex" -gt "4" ]]
+then
+	echo "Alerta! Explosão solar com K-index de:" $kindex
+	sleep 2
+	firefox http://services.swpc.noaa.gov/images/planetary-k-index.gif
+else
+	echo "NORMAL:" $kindex
+	sleep 2
+	firefox http://services.swpc.noaa.gov/images/planetary-k-index.gif
+fi
+
+
+
+
+
+
+
