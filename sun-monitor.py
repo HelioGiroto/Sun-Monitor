6b3f81f4d23444df4369cf1a3dd37309dfa479c7
Helio Giroto <31024350+HelioGiroto@users.noreply.github.com>
2017-09-11 17:12:32 -0300

    Create sun-monitor.py

---
+++ sun-monitor.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+#-*- coding: utf-8 -*-
+
+print("começando...")
+import requests, json
+
+endereco = "http://services.swpc.noaa.gov/products/noaa-planetary-k-index.json"
+
+pagina = requests.get(endereco)
+texto = pagina.text
+arqjson = json.loads(texto)
+
+
+kindex=arqjson[-1][1]
+
+print()
+print("Último registro:", arqjson[-1])
+print()
+print("Último K-index:", kindex)
